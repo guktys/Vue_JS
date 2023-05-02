66f3fb05e4980976b7c1357e83d75e881d05ff43
genia <guk.genia@gmail.com>
2023-05-02 12:42:17 +0300

    Все работает

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <i class="wi wi-snow" v-if="weather.list && weather.list[0].weather[0].main === 'Snow'"></i>
       <div class="search-box">
 
-        <input type="text" class="search-input" v-model="query" @keypress="fetchWeather" >
+        <input type="text" class="search-input" v-model="query" @keypress="fetchWeather">
       </div>
     </div>
     <div class="info-wrap" v-if="typeof weather.city != 'undefined'">
@@ -26,11 +26,17 @@
         <p class="details">{{ weather.list[0].weather[0].main }}</p>
 
       </div>
+      <div class="forecast-box">
+        <div v-for="(image, index) in forecastImages" :key="index"><p>{{ forecastDays[index] }}</p>
+
+          <p>{{ forecastWeather[index] }}°C</p>
+          <p><img :src="'http://openweathermap.org/img/w/' + image + '.png'"/></p>
+          <p>{{ forecastDetails[index] }}</p></div>
+      </div>
 
     </div>
 
   </div>
-
 
 
 </template>
@@ -42,20 +48,69 @@ import moment from 'moment';
 export default {
   name: 'App',
   data() {
-    return { api_key: 'a7b1ce98f00fbcb2aa1e794fbf8e8cb4',
+    return {
+      api_key: 'a7b1ce98f00fbcb2aa1e794fbf8e8cb4',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
     }
   },
   methods: {
-    fetchWeather (e) {
+    fetchWeather(e) {
       if (e.key === "Enter") {
-        fetch(`${this.url_base}forecast?q=${this.query}&units=metric&APPID=${this.api_key}&cnt=5`) .then(res => { return res.json();  }).then(this.setResults); } },
-    setResults (results) { this.weather = results;  },
-    dateBuilder () { let date = moment().format('MMMM Do YYYY'); return date; }
-  }
+        fetch(`${this.url_base}forecast?q=${this.query}&units=metric&APPID=${this.api_key}&cnt=5`).then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder() {
+      let date = moment().format('MMMM Do YYYY');
+      return date;
     }
+  },
+  computed: {
+    forecastDays() {
+      let days = [];
+      for (let i = 1; i <= 4; i++) {
+        days.push(moment().add(i, 'days').format("ddd"));
+      }
+      return days;
+    }, forecastWeather() {
+      let temperature = [];
+      for (let i = 1; i <= 4; i++) {
+        temperature.push(Math.round(this.weather.list[i].main.temp));
+      }
+      return temperature;
+    }, forecastImages() {
+      let images = [];
+      for (let i = 1; i <= 4; i++) {
+        images.push(this.weather.list[i].weather[0].icon);
+      }
+      return images;
+    }, forecastDetails() {
+      let details = [];
+      for (let i = 1; i <= 4; i++) {
+        details.push(this.weather.list[i].weather[0].main);
+      }
+      return details;
+    }
+
+  },
+  created() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        fetch(`${this.url_base}forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&APPID=${this.api_key}&cnt=5`).then(res => {
+          return res.json();
+        }).then(this.setResults);
+      });
+    } else {
+      console.log("Your browser does not support geolocation API")
+    }
+  },
+}
 
 </script>
 <style>
